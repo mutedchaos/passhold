78a900088625d4c1d9f19972b566008ab32f6a54
Tapani Haka <tapani.haka@barona.fi>
2020-11-01 23:59:00 +0200

    Use back to escape filter

--- packages/web/src/MainUI/MainUI.tsx
+++ packages/web/src/MainUI/MainUI.tsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react'
+import React, {useCallback, useState} from 'react'
+import {useBackButton} from '../UIComponents/goBackContext'
 import FilteredBrowser from './PasswordBrowser/FilteredBrowser/FilteredBrowser'
 import PasswordBrowser from './PasswordBrowser/PasswordBrowser'
 import PasswordFilter from './PasswordFilter'
 
 export default function MainUI() {
   const [filter, setFilter] = useState('')
+  const [filterReset, setFilterReset] = useState(0)
+
+  const clearFilter = useCallback(() => {
+    setFilterReset((x) => x + 1)
+    setFilter('')
+  }, [])
+
+  useBackButton(filter ? clearFilter : null)
 
   return (
     <div>
-      <PasswordFilter onFilter={setFilter} />
+      <PasswordFilter key={filterReset.toString()} onFilter={setFilter} />
       {filter ? <FilteredBrowser filter={filter} /> : <PasswordBrowser />}
     </div>
   )
